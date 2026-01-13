b03d1095088822a8bd726153c147e9209371b495
Oliver Metz <softwaredevdad@gmail.com>
2026-01-13 20:51:07 +0100

    Initial Project Setup with the nextjs FE and all shadcn ui components added.

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Serviq - Human-friendly AI",
+  description:
+    "Instantly resolve customer questions with an assistant that reads your docs and speaks with empathy",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
