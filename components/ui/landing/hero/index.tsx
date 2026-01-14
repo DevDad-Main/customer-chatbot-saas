cfaff36ae279dcb72997ff5e55d337f1bf81c0e1
Oliver Metz <softwaredevdad@gmail.com>
2026-01-14 09:48:36 +0100

    Update: add more to our hero section by adding in the visual chat. start to flesh out the chat section with avatars and chats.

--- components/ui/landing/hero/index.tsx
+++ components/ui/landing/hero/index.tsx
@@ -1,4 +1,5 @@
 import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 const Hero = () => {
@@ -32,6 +33,42 @@ const Hero = () => {
           <button className="h-11 px-8 cursor-pointer rounded-full border border-zinc-800 text-zinc-300 text-sm font-medium hover:border-zinc-600 hover:text-white transition-all bg-black/20 backdrop-blur-sm">
             View Demo
           </button>
+        </div>
+      </div>
+
+      {/* Visual Floating Chat Interface */}
+      <div className="max-w-3xl mx-auto relative z-10">
+        <div className="absolute inset-0 bg-indigo-500/10 blur-[100px] rounded-full pointer-events-none"></div>
+
+        <div className="rounded-2xl p-1 md:p-2 relative overflow-hidden ring-1 ring-white/10 bg-[#0a0a0e] shadow-2xl">
+          <div className="flex flex-col h-125 md:h-150 w-full bg-[#0a0a0e] rounded-xl overflow-hidden">
+            {/* Top Bar */}
+            <div className="h-14 border-b border-white/5 flex items-center justify-between px-6 bg-[#0e0e12] shrink-0">
+              <div className="flex items-center gap-2">
+                <div className="w-2 h-2 rounded-full bg-emerald-500 animate-pulse" />
+                <span className="text-sm font-medium text-zinc-300">
+                  ServIQ Inc.
+                </span>
+              </div>
+            </div>
+
+            {/* Chat Area */}
+            <div className="flex-1 p-6 overflow-y-auto space-y-6 bg-zinc-950/30">
+              <div className="flex w-full flex-col items-start">
+                <div className="w-10 h-10 rounded-full flex items-center justify-center">
+                  <Image
+                    src="/chatbot-avatar.jpg"
+                    alt="Support Agent"
+                    width={40}
+                    height={40}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+
+                <div className="space-y-2"></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
